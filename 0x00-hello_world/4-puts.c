e5a0c470c0f652a0361760f3c4dd4d1a3e6acae0
MehdiBouchiha <bouchihamehdi409@gmail.com>
2023-06-15 17:22:29 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+    puts("\"Programming is like building a multilingual puzzle");
+
+    return 0;
+}
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("with proper grammar, but the outcome is a piece of art,\n");
+
+    return 0;
+}
