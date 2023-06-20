32c6266b5e05fea3e2ac9296a1aba8d2ff5628ee
MehdiBouchiha <bouchihamehdi409@gmail.com>
2023-06-20 08:30:36 -0700

    Readme file created

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,10 @@
-#include <unistd.h>
+#include "main.h"
 
-int _putchar(char c)
-{
-  return write(1, &c, 1);
-}
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+
 
 int main(void)
 {
@@ -18,4 +19,3 @@ int main(void)
   _putchar('\n');
   return (0);
 }
-
