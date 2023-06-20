0f3a63debf24a3a0518b13693a552dedef58fe69
MehdiBouchiha <bouchihamehdi409@gmail.com>
2023-06-20 09:09:20 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,21 @@
-#include "main.h"
+#include <unistd.h>
 
-/**
- * main - Entry point of my code,
- *
- * Return: Always 0
- */
+int _putchar(char c)
+{
+  return write(1, &c, 1);
+}
 
 int main(void)
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
-return (0);
+  _putchar('_');
+  _putchar('p');
+  _putchar('u');
+  _putchar('t');
+  _putchar('c');
+  _putchar('h');
+  _putchar('a');
+  _putchar('r');
+  _putchar('\n');
+  return 0;
 }
+
