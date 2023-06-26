e2a8fe23a22253b585711e0d221cc61924ebbd83
MehdiBouchiha <bouchihamehdi409@gmail.com>
2023-06-26 10:46:23 -0700

    3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,15 +1,21 @@
 #include <unistd.h>
 
+/**
+ * _puts - prints a string
+ * @str: The string to print
+ * Return: void
+ */
+
 void _puts(char *str)
 {
-    int i = 0;
+int i = 0;
 
-    while (str[i] != '\0')
-    {
-        write(1, &str[i], 1);
-        i++;
-    }
+while (str[i] != '\0')
+{
+write(1, &str[i], 1);
+i++;
+}
 
-    write(1, "\n", 1);
+write(1, "\n", 1);
 }
 
